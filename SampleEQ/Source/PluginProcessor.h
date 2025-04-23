47173ea089533ef94a779e95208875b14d0e6bab
Tyz kotono <2713421980@qq.com>
2025-04-23 17:54:38 +0800

    UI Parameter for Frequency

--- SampleEQ/Source/PluginProcessor.h
+++ SampleEQ/Source/PluginProcessor.h
@@ -53,6 +53,16 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+    //Kotono
+    //https://docs.juce.com/master/classAudioProcessorParameter.html
+    static juce::AudioProcessorValueTreeState::ParameterLayout CreateParameterLayout();
+    juce::AudioProcessorValueTreeState apvts
+    {
+        *this,
+        nullptr,
+        "Paramters",
+        CreateParameterLayout(),
+    };
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SampleEQAudioProcessor)
